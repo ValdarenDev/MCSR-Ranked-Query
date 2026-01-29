573416fc5c186aba37b8ec90636c21046c8a841c
ValdarenDev <bdog0804@gmail.com>
2026-01-29 11:12:40 -0600

    Removing local hosting from index.js

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
-
 let queryResult;
 let queryType;
 
