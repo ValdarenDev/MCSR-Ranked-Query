2a90740d7a73f5dead937108adb9bad9526712ce
ValdarenDev <bdog0804@gmail.com>
2026-01-29 11:22:17 -0600

    Allowing Vercel to see CSS

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json());
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
 let queryResult;
 let queryType;
